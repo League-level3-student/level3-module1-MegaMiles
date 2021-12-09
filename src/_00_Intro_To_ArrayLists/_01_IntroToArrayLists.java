14669822bfcdfd6c3518aad5e0a3048dfcb72972
MegaMiles <55776997+MegaMiles@users.noreply.github.com>
2021-12-09 15:28:20 -0800

    GuestBook.java

--- src/_00_Intro_To_ArrayLists/_01_IntroToArrayLists.java
+++ src/_00_Intro_To_ArrayLists/_01_IntroToArrayLists.java
@@ -1,18 +1,36 @@
 package _00_Intro_To_ArrayLists;
 
+import java.util.ArrayList;
+
 public class _01_IntroToArrayLists {
     public static void main(String[] args) {
         // 1. Create an array list of Strings
         //    Don't forget to import the ArrayList class
-
+ArrayList<String> strings = new ArrayList<String>();
         // 2. Add five Strings to your list
-
+strings.add("#1string");
+strings.add("#2string");
+strings.add("#3string");
+strings.add("#4string");
+strings.add("#5string");
         // 3. Print all the Strings using a standard for-loop
-
+for (int i = 0; i < strings.size(); i++) {
+	 String s = strings.get(i);
+}
         // 4. Print all the Strings using a for-each loop
-
+for(String s : strings){
+ 
+}
         // 5. Print only the even numbered elements in the list.
-
+for (int i = 0; i < strings.size(); i++) {
+if (i % 2 == 1) {
+System.out.println(strings.get(i));	
+}
+if (i % 2 == 0) {
+	System.out.println("Not an even number");
+}	
+	
+}
         // 6. Print all the Strings in reverse order.
 
         // 7. Print only the Strings that have the letter 'e' in them.
--- src/_00_Intro_To_ArrayLists/_02_GuestBook.java
+++ src/_00_Intro_To_ArrayLists/_02_GuestBook.java
@@ -1,6 +1,14 @@
 package _00_Intro_To_ArrayLists;
 
-public class _02_GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class _02_GuestBook implements ActionListener {
     /*
      * Create a GUI with two buttons. One button reads "Add Name" and the other
      * button reads "View Names". When the add name button is clicked, display
@@ -13,5 +21,51 @@ public class _02_GuestBook {
      * Guest #3: Greg Ganders
      * Guest #4: Donny Doners
      */
+	JButton button1 = new JButton("Add name");
+	JButton button2 = new JButton("View names");
+public static void main(String[] args) {
+_02_GuestBook book = new _02_GuestBook();
+book.ActionListeners();
+}
+public void ActionListeners() {
+	//creating frame, panel, and buttons
+JFrame frame = new JFrame();	
+JPanel panel = new JPanel();
+
+//adding uses for the frame,panel, and buttons	
+frame.add(panel);
+panel.add(button1);
+panel.add(button2);
+frame.setVisible(true);
+button1.addActionListener(this);
+button1.addActionListener(this);
 
 }
+@Override
+public void actionPerformed(ActionEvent e) {
+//logic and stuff
+if (e.getSource() == button1) {
+String names = JOptionPane.showInputDialog("Please enter a name to the guest book");		
+}
+if (e.getSource() == button2) {
+JOptionPane.showMessageDialog(null,"Here are the names in the guest book as follows ");	
+JOptionPane.showMessageDialog(null,"Guest #1 is " + );	
+}
+
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
