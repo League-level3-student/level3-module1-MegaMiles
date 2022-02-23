3de961e6543300f106080bc25001de0b7a8cd202
MegaMiles <55776997+MegaMiles@users.noreply.github.com>
2022-02-22 16:58:37 -0800

    CaliforniaWeather.java

--- src/_08_California_Weather/CaliforniaWeather.java
+++ src/_08_California_Weather/CaliforniaWeather.java
@@ -44,6 +44,8 @@ public class CaliforniaWeather implements ActionListener {
 		WeatherSearch.addActionListener(this);
 		WeatherSpecify.addActionListener(this);
 		WeatherMinMax.addActionListener(this);
+		
+		
 
 //  2. Create a way for the user to specify the weather condition and then
 //  list the cities that have those conditions.
@@ -56,43 +58,58 @@ public class CaliforniaWeather implements ActionListener {
 //           Program: Fortana, Glendale, Escondido, Del Mar, ...
 
 	}
+	
 // EXTRA:
 //  Feel free to add pictures for specific weather conditions or a thermometer
 //  for the temperature. Also If you want your program to get the current day's
 //  temperature, you can get a free API key at: https://openweathermap.org/api
+	HashMap<String, WeatherData> weatherData = Utilities.getWeatherData();
 
-	void start() {
-		HashMap<String, WeatherData> weatherData = Utilities.getWeatherData();
-
-		// All city keys have the first letter capitalized of each word
-		String cityName = Utilities.capitalizeWords("San Diego");
-		WeatherData datum = weatherData.get(cityName);
-
-		if (datum == null) {
-			System.out.println("Unable to find weather data for: " + cityName);
-		} else {
-			System.out.println(
-					cityName + " is " + datum.weatherSummary + " with a temperature of " + datum.temperatureF + " F");
-		}
-	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == WeatherSearch) {
 			String Search = JOptionPane.showInputDialog(null, "Please enter the city for information on the conditions of the city");
-			for (String WeatherVAR : weatherData.keySet()) {
-				if (weatherData.contains(Search)) {
-
+			String cityName = Utilities.capitalizeWords(Search);
+			WeatherData datum = weatherData.get(cityName);
+				if (datum == null) {
+JOptionPane.showMessageDialog(null, "Unable to find weather data for: " + cityName);
+				} else {
+JOptionPane.showMessageDialog(null, cityName + " is " + datum.weatherSummary + " with a temperature of " + datum.temperatureF + " F");
 				}
+	
 			}
-		}
+
 		if (e.getSource() == WeatherSpecify) {
-			String Specify = JOptionPane.showInputDialog(null,
-					"Please specify the weather conditions of the city you are look for(Hot, Cold, Cloudy, Raining, Snowing, Clear");
+			String Specify = JOptionPane.showInputDialog(null, "Please specify the weather conditions of the city you are look for");
+for (String WeatherCity : weatherData.keySet()) {
+	WeatherData WD = weatherData.get(WeatherCity);
+	System.out.println(WD.weatherSummary);
+}	
+if (!weatherData.containsKey(Specify)) {
+	JOptionPane.showMessageDialog(null, "Unable to find a city with the condition " + "'" + Specify + "'. Please make sure you typed it in correctly and try again.");
+}
+
+
+			
+			
+			
+			
 		}
+		
 		if (e.getSource() == WeatherMinMax) {
 			String MinMax = JOptionPane.showInputDialog(null,
 					"Please enter the minimum and maximum of the city you are looking for like this x, y");
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
 		}
 
 	}
